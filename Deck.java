0a1694d8cbc055f29b8198ca5a98a80a9ccdc72b
parnello <aparnello97@gmail.com>
2015-05-06 09:27:00 -0700

    Finished Deck constructor

--- Deck.java
+++ Deck.java
@@ -7,7 +7,12 @@ public class Deck {
    public Deck(){
       deck = new ArrayList<Card>();
       //TODO: Add a card (2-14) of each suit (h,s,d,c)
-      
+      for(int i = 2; i < 15; i++) {
+        deck.add(new Card(i, "h"));
+        deck.add(new Card(i, "s"));
+        deck.add(new Card(i, "d"));
+        deck.add(new Card(i, "c"));
+      }
    }
 
    public Card remove(){
